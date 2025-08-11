aba6410697cc4c677a4171c6b86bcdddfa6d7032
YDevam <devamyadav147@gmail.com>
2025-08-11 23:30:16 +0530

    first commit

---
+++ Cal.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int cal(int n)
+{
+    int a, b, sum;
+    printf("Enter value of a = ");
+    scanf("%d", &a);
+    printf("Enter value of b = ");
+    scanf("%d", &b);
+    switch (n)
+    {
+    case 1:
+        sum = a + b;
+        return printf("\nAddition of a and b = \t%d\n", sum);
+        break;
+
+    case 2:
+        sum = a - b;
+        return printf("\nSubtraction of a and b = \t%d\n", sum);
+        break;
+    case 3:
+        sum = a * b;
+        return printf("\nMultiplication of a and b = \t%d\n", sum);
+        break;
+    case 4:
+        sum = a / b;
+        return printf("\nDivision of a and b = \t%d\n", sum);
+        break;
+    case 5:
+        sum = a % b;
+        return printf("\nModulus of a and b = \t%d\n", sum);
+        break;
+    };
+}
+int main()
+{
+    int j;
+    for (int i = 0; i < 1000; i++)
+    {
+        // printf("Enter value ");
+        printf("\nPress 1 for Addition");
+        printf("\nPress 2 for Subtraction");
+        printf("\nPress 3 for Multiplication");
+        printf("\nPress 4 for Division");
+        printf("\nPress 5 for Modulus");
+        printf("\nPress 0 for Exit \n");
+        scanf("%d", &j);
+        if (j > 0)
+        {
+            cal(j);
+            j == 0;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
